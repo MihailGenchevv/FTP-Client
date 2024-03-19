ac37ca1d7cdd0c59de6fa38b9d6d2ce90196a5e3
MihailGenchev <mihail@investotech.com>
2024-03-19 15:04:50 +0200

    more exercises

--- hello.cpp
+++ hello.cpp
@@ -38,7 +38,126 @@ int mostOccurringElement() {
 }
 
 
+
+int reverseArray() {
+    int n;
+    cin >> n;
+
+    int arr[MAX_SIZE];
+
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+
+    for (int i = 0; i < n / 2; ++i) {
+        swap(arr[i], arr[n-i-1]);
+    }
+
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    return 1;
+}
+
+
+int mergeSortedArrays(){
+    int n1, n2;
+    cin >> n1;
+    int arr1[MAX_SIZE], arr2[MAX_SIZE], result[MAX_SIZE];
+    for (int i = 0; i < n1; ++i) {
+        cin >> arr1[i];
+    }
+    cin >> n2;
+    for (int i = 0; i < n2; ++i) {
+        cin >> arr2[i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < n1 and j < n2){
+        if (arr1[i] < arr2[j]){
+            result[k] = arr1[i];
+            i++;
+        } else {
+            result[k] = arr2[j];
+            j++;
+        }
+        k++;
+
+    }
+
+    while( i < n1) {
+        result[k++] = arr1[i++];
+    }
+
+    while (j < n2) {
+        result[k++] = arr2[j++];
+    }
+
+    for (int i = 0; i < n1 + n2; i++) {
+        cout << result[i] << " ";
+    }
+    return 1;
+}
+
+
+int numberToBinary() {
+    int k;
+
+    cin >> k;
+
+    int arr[MAX_SIZE];
+
+    int s = 0;
+    while (k != 0) {
+        arr[s] = k % 2;
+        k = k / 2;
+        s++;
+    }
+
+
+    for (int i = 0; i < s/2; ++i) {
+        swap(arr[i], arr[s-i-1]);
+    }
+
+    for (int i = 0; i < s; i++ ){
+        cout << arr[i] << " ";
+    }
+
+    return 1;
+}
+
+
+int generateAllSubsequenceOfArray(){
+
+    int n;
+
+    int arr[MAX_SIZE];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int k = 1;  k  <= n; k++ ){
+        for (int j = 0; j + k <= n; j++){
+            for (int i = j; i < j + k; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+
+    }
+
+}
+
+
 int main() {
 
-    mostOccurringElement();
+    generateAllSubsequenceOfArray();
 }
